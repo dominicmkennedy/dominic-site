fb9e2158d775db512c5fbfdc0673294422e72e35
dom <dominicmkennedy@gmail.com>
2023-12-21 01:22:47 -0500

    move album compotent off main route

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -13,8 +13,8 @@ const getArtists = (y: IArtistCredit[] | undefined) => {
 }
 
 export const load: PageLoad = async () => {
-  const mbid = "f6f862f0-eae6-4ba1-a27a-352e82f0a6a5"
-  // const mbid = "09f8b11c-dfaa-4259-842e-0f10c25155fe"
+  // const mbid = "f6f862f0-eae6-4ba1-a27a-352e82f0a6a5"
+  const mbid = "09f8b11c-dfaa-4259-842e-0f10c25155fe"
   // const mbid = "f5ffc4db-fdef-4d0d-bf5e-f9f1bc5fef3b"
 
 
