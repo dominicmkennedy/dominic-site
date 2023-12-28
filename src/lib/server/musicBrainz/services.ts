308a0baa3ff1dfaa4aeb5a71e0e669573ced6830
dom <dominicmkennedy@gmail.com>
2023-12-28 16:18:33 -0500

    large db overhaul

--- src/lib/server/musicBrainz/services.ts
+++ src/lib/server/musicBrainz/services.ts
@@ -1,5 +1,7 @@
 import type { IArtistCredit } from 'musicbrainz-api';
 import { mbApi } from './config';
+import type { Album, Track } from '@prisma/client';
+import { std, mean, sum } from 'mathjs';
 
 const getArtists = (y: IArtistCredit[] | undefined) => {
   return y ? y.map(x => `${x.name}${x.joinphrase ? ', ' : ''}`).join('') : ''
@@ -7,38 +9,42 @@ const getArtists = (y: IArtistCredit[] | undefined) => {
 
 export const getAlbumData = async (mbid: string) => {
   const mbidData = await mbApi.lookup('release', mbid, ['recordings', 'artist-credits'])
-  const url: string = (
-    await (
-      await fetch(
-        `https://coverartarchive.org/release/${mbid}`
-      )
-    ).json()
-  ).images[0].thumbnails['250'];
 
-  const tracks = mbidData.media
+  const trackDurations = mbidData.media
     .map(x => x.tracks)
     .flat()
-    .map((x,i) => ({
+    .map(x => x.recording.length)
+
+  const trackLenMean = mean(trackDurations)
+  const trackLenStd: number = std(trackDurations)
+
+  const tracks: Track[] = mbidData.media
+    .map(x => x.tracks)
+    .flat()
+    .map((x, i) => ({
       mbid: x.id,
       albumId: mbid,
       name: x.recording.title,
       credits: getArtists(x['artist-credit']),
       duration: x.recording.length,
       trackNumber: i,
+      zScore: (x.recording.length - trackLenMean) / trackLenStd,
       relAlbumScore: null
     }))
 
-  const album = {
+  const album: Album = {
     mbid,
     name: mbidData.title,
     credits: getArtists(mbidData['artist-credit']),
-    duration: tracks.map(x => x.duration).reduce((x, y) => x + y, 0),
-    numberTracks: tracks.length,
+    duration: sum(trackDurations),
+    trackLenMean,
+    trackLenStd,
     releaseDate: new Date(mbidData.date),
-    coverUrl: url,
+    coverArtUrl: `https://coverartarchive.org/release/${mbid}/front-250`,
+    reviewTextMd: null,
     dominicScore: null,
     currentElo: null,
-    reviewText: null,
+    reviewDate: null,
   }
 
   return { album, tracks };
