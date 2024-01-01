105639ba4e7c2502a925a375ee4294c1c7e1c6ab
dom <dominicmkennedy@gmail.com>
2024-01-01 17:34:35 -0500

    update landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,10 +4,10 @@
 	const source = `
 # Hello!
 My name is Dominic Kennedy, and this is my website.
-This site is where I plan to be most active, so if you'd like to keep up with the goings on in my life, this is the place to be.
-It is still very much under construction, which is why you should check back soon for more content \`;)\`.
+This is where I plan to be most active, so if you'd like to keep up with the goings on in my life, you're in the right place.
+The site is still very much under construction, which is why you should check back soon for more content \`;)\`.
 
-The source code for this website is hosted [here](https://github.com/dominicmkennedy/dominic-site).
+[Source code](https://github.com/dominicmkennedy/dominic-site) for this site.
   `;
 </script>
 
