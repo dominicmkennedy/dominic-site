ef99fc90c293210a2503e8b3b3cf98d02d140453
dom <dominicmkennedy@gmail.com>
2024-01-06 02:33:55 -0500

    move old server routes

--- src/routes/music/addAlbum/+server.ts
+++
@@ -1,8 +0,0 @@
-import type { RequestHandler } from './$types';
-import { addAlbum } from '$lib/server/db/services';
-import { json } from '@sveltejs/kit';
-
-export const POST: RequestHandler = async ({ url }) => {
-  await addAlbum(url.searchParams.get('mbid')!)
-  return json({})
-};
