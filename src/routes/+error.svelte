8b5e7c8ab83bdd1b2be417aa898adf33e299de2f
dom <dominicmkennedy@gmail.com>
2024-01-06 21:48:36 -0500

    add error route

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<div class="h-full flex justify-center items-center">
+	<div class="space-y-4">
+		<h1 class="h1">Error: {$page.status}</h1>
+		<h2 class="h2">{$page.error?.message}</h2>
+	</div>
+</div>
